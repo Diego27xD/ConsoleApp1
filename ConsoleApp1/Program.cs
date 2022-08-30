41fc1653ac85bf3548e5698e3cf34d92c703d120
Diego Flores <floresdiego2001xd@gmail.com>
2022-08-30 18:24:34 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicoCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Declaración variablaes
+            //Clases
+
+            Console.WriteLine("CALCULO DE AREA Y PERIMETRO DE UN TRIANGULO");
+
+            //SE SOLICITA EL INGRESO DE LOS 8 PUNTOS PARA REALIZAR LAS OPERACIONES
+            Console.WriteLine("Ingrese Valor X1");
+            string? valor1 = Console.ReadLine();
+
+            
+            Console.WriteLine("Ingrese Valor Y1");
+            string? valor2 = Console.ReadLine();
+
+            
+            Console.WriteLine("Ingrese Valor X2");
+            string? valor3 = Console.ReadLine();
+
+           
+            Console.WriteLine("Ingrese Valor Y2");
+            string? valor4 = Console.ReadLine();
+
+            
+            Console.WriteLine("Ingrese Valor X3");
+            string? valor5 = Console.ReadLine();
+
+           
+            Console.WriteLine("Ingrese Valor Y3");
+            string? valor6 = Console.ReadLine();
+
+            
+            Console.WriteLine("Ingrese Valor X4");
+            string? valor7 = Console.ReadLine();
+
+            
+            Console.WriteLine("Ingrese Valor Y4");
+            string? valor8 = Console.ReadLine();
+
+            //SE ASIGNAN LOS VALORES X y Y A CADA VERTICE (CLASE VERTICE)
+            Vertice v1 = new Vertice
+            {
+                x = Int32.Parse(valor1), 
+                y = Int32.Parse(valor2)
+            };
+            Vertice v2 = new Vertice
+            {
+                x = Int32.Parse(valor3),
+                y = Int32.Parse(valor4)
+            };
+            Vertice v3 = new Vertice
+            {
+                x = Int32.Parse(valor5),
+                y = Int32.Parse(valor6)
+            };
+            Vertice v4 = new Vertice
+            {
+                x = Int32.Parse(valor7),
+                y = Int32.Parse(valor8)
+            };
+
+            //SE ASIGNAN LOS VERTICES AL RECTÁNGULO (CLASE RECTANGULO)
+            Rectangulo rectangulo = new Rectangulo();
+
+            rectangulo.v1 = v1;
+            rectangulo.v2 = v2;
+            rectangulo.v3 = v3;
+            rectangulo.v4 = v4;
+            
+            //SE MUESTRAN LOS RESULTADOS
+            Console.WriteLine("El area del rectangulo es: " + rectangulo.GetAreaRectangulo() + "u^2");
+            Console.WriteLine("El perimetro del rectangulo es: " + rectangulo.GetPerimetroRectangulo() + "u");
+
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ ConsoleApp1/Rectangulo.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Rectangulo
+    {
+        public Vertice v1 { get; set; }
+        public Vertice v2 { get; set; }
+        public Vertice v3 { get; set; }
+        public Vertice v4 { get; set; }
+
+        public double area { get; set; }
+        public double perimetro { get; set; }
+
+        public double GetAreaRectangulo()
+        {
+            double baseTriangulo = Math.Sqrt(Math.Pow(v2.x - v1.x, 2) + Math.Pow(v2.y - v1.y, 2));
+            double alturaTriangulo = Math.Sqrt(Math.Pow(v3.x - v2.x, 2) + Math.Pow(v3.y - v2.y, 2));
+            area = baseTriangulo * alturaTriangulo;
+            return area;
+        }
+
+        public double GetPerimetroRectangulo()
+        {
+            double baseInferior = Math.Sqrt(Math.Pow((v2.x - v1.x), 2) + Math.Pow((v2.y - v1.y), 2));
+            double baseSuperior = Math.Sqrt(Math.Pow((v4.x - v3.x), 2) + Math.Pow((v4.y - v3.y), 2));
+            double ladoIzquiero = Math.Sqrt(Math.Pow((v4.x - v1.x), 2) + Math.Pow((v4.y - v1.y), 2));
+            double ladoDerecho = Math.Sqrt(Math.Pow((v3.x - v2.x), 2) + Math.Pow((v3.y - v2.y), 2));
+            perimetro = baseInferior + ladoIzquiero + baseSuperior + ladoDerecho;
+            return perimetro;
+        }
+    }
+}
